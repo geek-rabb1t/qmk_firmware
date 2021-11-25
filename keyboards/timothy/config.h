99fb23f573482a6785afc03733ead74c3c174384
geek-rabb1t <91539491+geek-rabb1t@users.noreply.github.com>
2021-11-25 21:07:47 +0900

    Change configuration for the original PCB

--- keyboards/timothy/config.h
+++ keyboards/timothy/config.h
@@ -40,8 +40,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
  */
-#define MATRIX_ROW_PINS { F6, F7, B1, B3, B2 }
-#define MATRIX_COL_PINS { D1, D0, D4, C6, D7, E6, B4 }
+#define MATRIX_ROW_PINS { C6, E6, D7, B3, B6 }
+#define MATRIX_COL_PINS { F5, B1, F6, D4, B5, B4, D1 }
+// #define MATRIX_COL_PINS { F5, B1, F6, D4, B5, B4, B2 }  // TEST for I2C
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL */
@@ -50,7 +51,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /*
  * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
  */
-#define SOFT_SERIAL_PIN D2
+#define SOFT_SERIAL_PIN D2  // or D1, D2, D3, E6
+#define SPLIT_HAND_PIN D0
+// #define USE_I2C
 
 //#define LED_NUM_LOCK_PIN B0
 //#define LED_CAPS_LOCK_PIN B1
@@ -62,33 +65,94 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define BACKLIGHT_LEVELS 3
 //#define BACKLIGHT_BREATHING
 
-//#define RGB_DI_PIN E2
-//#ifdef RGB_DI_PIN
-//#    define RGBLED_NUM 16
-//#    define RGBLIGHT_HUE_STEP 8
-//#    define RGBLIGHT_SAT_STEP 8
-//#    define RGBLIGHT_VAL_STEP 8
-//#    define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
-//#    define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
-/*== all animations enable ==*/
+#define RGB_DI_PIN F4
+#ifdef RGB_DI_PIN
+//*
+// RGB Lighting
+#    define RGBLED_NUM 82
+#    define RGBLIGHT_SPLIT
+#    define RGBLIGHT_SLEEP
 //#    define RGBLIGHT_ANIMATIONS
-/*== or choose animations ==*/
+//#    define RGBLIGHT_EFFECT_ALTERNATING
 //#    define RGBLIGHT_EFFECT_BREATHING
+//#    define RGBLIGHT_EFFECT_CHRISTMAS
+//#    define RGBLIGHT_EFFECT_KNIGHT
 //#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
 //#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-//#    define RGBLIGHT_EFFECT_SNAKE
-//#    define RGBLIGHT_EFFECT_KNIGHT
-//#    define RGBLIGHT_EFFECT_CHRISTMAS
-//#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
 //#    define RGBLIGHT_EFFECT_RGB_TEST
-//#    define RGBLIGHT_EFFECT_ALTERNATING
-/*== customize breathing effect ==*/
-/*==== (DEFAULT) use fixed table instead of exp() and sin() ====*/
-//#    define RGBLIGHT_BREATHE_TABLE_SIZE 256      // 256(default) or 128 or 64
-/*==== use exp() and sin() ====*/
-//#    define RGBLIGHT_EFFECT_BREATHE_CENTER 1.85  // 1 to 2.7
-//#    define RGBLIGHT_EFFECT_BREATHE_MAX    255   // 0 to 255
-//#endif
+//#    define RGBLIGHT_EFFECT_SNAKE
+//#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+//#    define RGBLIGHT_EFFECT_TWINKLE
+
+
+#    define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_STATIC_LIGHT
+#    define RGBLIGHT_DEFAULT_HUE 64
+#    define RGBLIGHT_LIMIT_VAL 64
+#    define RGBLIGHT_DEFAULT_VAL RGBLIGHT_LIMIT_VAL
+/*/
+// RGB Matrix
+#    define DRIVER_LED_TOTAL 68
+#    define RGB_MATRIX_KEYPRESSES
+#    define SPLIT_TRANSPORT_MIRROR
+
+#    define RGB_MATRIX_FRAMEBUFFER_EFFECTS // enable framebuffer effects
+#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 20 // limits maximum brightness of LEDs to 200 out of 255. If not defined maximum brightness is set to 255
+#    define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_TYPING_HEATMAP // Sets the default mode, if none has been set
+#    define RGB_DISABLE_TIMEOUT 60000 // number of milliseconds to wait until rgb automatically turns off
+//#define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
+#    define RGB_MATRIX_STARTUP_HUE 0 // Sets the default hue value, if none has been set
+#    define RGB_MATRIX_STARTUP_SAT 0 // Sets the default saturation value, if none has been set
+// #define RGB_MATRIX_STARTUP_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS // Sets the default brightness value, if none has been set
+#    define RGB_MATRIX_STARTUP_SPD 127 // Sets the default animation speed, if none has been set
+// #define RGB_MATRIX_DISABLE_KEYCODES // disables control of rgb matrix by keycodes (must use code functions to control the feature)
+#define RGB_MATRIX_SPLIT { 34, 34 }     // (Optional) For split keyboards, the number of LEDs connected on each half. X = left, Y = Right.
+                                      // If RGB_MATRIX_KEYPRESSES or RGB_MATRIX_KEYRELEASES is enabled, you also will want to enable SPLIT_TRANSPORT_MIRROR
+
+#    define DISABLE_RGB_MATRIX_ALPHAS_MODS
+#    define DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+#    define DISABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
+#    define DISABLE_RGB_MATRIX_BREATHING
+#    define DISABLE_RGB_MATRIX_BAND_SAT
+#    define DISABLE_RGB_MATRIX_BAND_VAL
+#    define DISABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
+#    define DISABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
+#    define DISABLE_RGB_MATRIX_BAND_SPIRAL_SAT
+#    define DISABLE_RGB_MATRIX_BAND_SPIRAL_VAL
+#    define DISABLE_RGB_MATRIX_CYCLE_ALL
+#    define DISABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+#    define DISABLE_RGB_MATRIX_CYCLE_UP_DOWN
+#    define DISABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+#    define DISABLE_RGB_MATRIX_CYCLE_OUT_IN
+#    define DISABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+#    define DISABLE_RGB_MATRIX_CYCLE_PINWHEEL
+#    define DISABLE_RGB_MATRIX_CYCLE_SPIRAL
+#    define DISABLE_RGB_MATRIX_DUAL_BEACON
+#    define DISABLE_RGB_MATRIX_RAINBOW_BEACON
+#    define DISABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+
+#    define DISABLE_RGB_MATRIX_RAINDROPS
+#    define DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+
+#    define DISABLE_RGB_MATRIX_HUE_BREATHING
+#    define DISABLE_RGB_MATRIX_HUE_PENDULUM
+#    define DISABLE_RGB_MATRIX_HUE_WAVE
+
+//#    define DISABLE_RGB_MATRIX_TYPING_HEATMAP
+#    define DISABLE_RGB_MATRIX_DIGITAL_RAIN
+#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE
+#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+#    define DISABLE_RGB_MATRIX_SPLASH
+#    define DISABLE_RGB_MATRIX_MULTISPLASH
+#    define DISABLE_RGB_MATRIX_SOLID_SPLASH
+//#    define DISABLE_RGB_MATRIX_SOLID_MULTISPLASH
+//*/
+#endif
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
@@ -150,3 +214,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Bootmagic Lite key configuration */
 //#define BOOTMAGIC_LITE_ROW 0
 //#define BOOTMAGIC_LITE_COLUMN 0
+
+
+// #define WS2812_ADDRESS 0xb0 // default: 0xb0
+// #define WS2812_TIMEOUT 100 // default: 100
