920d2769e34a921c21c464540d7746d1db74d8a5
geek-rabb1t <91539491+geek-rabb1t@users.noreply.github.com>
2022-09-13 22:51:40 +0900

    Update keymap

--- keyboards/timothy/config.h
+++ keyboards/timothy/config.h
@@ -40,8 +40,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
  */
-#define MATRIX_ROW_PINS { C6, E6, D7, B3, B6 }
-#define MATRIX_COL_PINS { F5, B1, F6, D4, B5, B4, D1 }
+#define MATRIX_ROW_PINS { F7, B1, B3, B2, B6 }
+#define MATRIX_COL_PINS { D0, D4, C6, D7, E6, B4, B5 }
 // #define MATRIX_COL_PINS { F5, B1, F6, D4, B5, B4, B2 }  // TEST for I2C
 #define UNUSED_PINS
 
@@ -52,7 +52,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
  */
 #define SOFT_SERIAL_PIN D2  // or D1, D2, D3, E6
-#define SPLIT_HAND_PIN D0
+#define SPLIT_HAND_PIN F4
 // #define USE_I2C
 
 //#define LED_NUM_LOCK_PIN B0
@@ -65,14 +65,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define BACKLIGHT_LEVELS 3
 //#define BACKLIGHT_BREATHING
 
-#define RGB_DI_PIN F4
+#define RGB_DI_PIN F5
 #ifdef RGB_DI_PIN
-//*
+/*
 // RGB Lighting
-#    define RGBLED_NUM 82
-#    define RGBLIGHT_SPLIT
+#    define RGBLED_NUM 66
+//#    define RGBLIGHT_SPLIT
 #    define RGBLIGHT_SLEEP
-//#    define RGBLIGHT_ANIMATIONS
+#    define RGBLIGHT_ANIMATIONS
 //#    define RGBLIGHT_EFFECT_ALTERNATING
 //#    define RGBLIGHT_EFFECT_BREATHING
 //#    define RGBLIGHT_EFFECT_CHRISTMAS
@@ -87,70 +87,73 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #    define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_STATIC_LIGHT
 #    define RGBLIGHT_DEFAULT_HUE 64
-#    define RGBLIGHT_LIMIT_VAL 64
-#    define RGBLIGHT_DEFAULT_VAL RGBLIGHT_LIMIT_VAL
+#    define RGBLIGHT_LIMIT_VAL 128
+#    define RGBLIGHT_DEFAULT_VAL 32
 /*/
 // RGB Matrix
-#    define DRIVER_LED_TOTAL 68
+
+// #    define RGBLED_NUM 66
+#    define DRIVER_LED_TOTAL 66
 #    define RGB_MATRIX_KEYPRESSES
-#    define SPLIT_TRANSPORT_MIRROR
+//#    define SPLIT_TRANSPORT_MIRROR
 
 #    define RGB_MATRIX_FRAMEBUFFER_EFFECTS // enable framebuffer effects
-#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 20 // limits maximum brightness of LEDs to 200 out of 255. If not defined maximum brightness is set to 255
-#    define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_TYPING_HEATMAP // Sets the default mode, if none has been set
-#    define RGB_DISABLE_TIMEOUT 60000 // number of milliseconds to wait until rgb automatically turns off
-//#define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
-#    define RGB_MATRIX_STARTUP_HUE 0 // Sets the default hue value, if none has been set
-#    define RGB_MATRIX_STARTUP_SAT 0 // Sets the default saturation value, if none has been set
+#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 200 // limits maximum brightness of LEDs to 200 out of 255. If not defined maximum brightness is set to 255
+#    define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_TYPING_HEATMAP  // Sets the default mode, if none has been set
+#    define RGB_DISABLE_TIMEOUT 600000 // number of milliseconds to wait until rgb automatically turns off
+#    define RGB_DISABLE_WHEN_USB_SUSPENDED true// turn off effects when suspended
+#    define RGB_MATRIX_STARTUP_HUE 64 // Sets the default hue value, if none has been set
+#    define RGB_MATRIX_STARTUP_SAT 255 // Sets the default saturation value, if none has been set
 // #define RGB_MATRIX_STARTUP_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS // Sets the default brightness value, if none has been set
 #    define RGB_MATRIX_STARTUP_SPD 127 // Sets the default animation speed, if none has been set
 // #define RGB_MATRIX_DISABLE_KEYCODES // disables control of rgb matrix by keycodes (must use code functions to control the feature)
-#define RGB_MATRIX_SPLIT { 34, 34 }     // (Optional) For split keyboards, the number of LEDs connected on each half. X = left, Y = Right.
+//#define RGB_MATRIX_SPLIT { 33, 33 }     // (Optional) For split keyboards, the number of LEDs connected on each half. X = left, Y = Right.
                                       // If RGB_MATRIX_KEYPRESSES or RGB_MATRIX_KEYRELEASES is enabled, you also will want to enable SPLIT_TRANSPORT_MIRROR
 
-#    define DISABLE_RGB_MATRIX_ALPHAS_MODS
-#    define DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN
-#    define DISABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
-#    define DISABLE_RGB_MATRIX_BREATHING
-#    define DISABLE_RGB_MATRIX_BAND_SAT
-#    define DISABLE_RGB_MATRIX_BAND_VAL
-#    define DISABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
-#    define DISABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
-#    define DISABLE_RGB_MATRIX_BAND_SPIRAL_SAT
-#    define DISABLE_RGB_MATRIX_BAND_SPIRAL_VAL
-#    define DISABLE_RGB_MATRIX_CYCLE_ALL
-#    define DISABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
-#    define DISABLE_RGB_MATRIX_CYCLE_UP_DOWN
-#    define DISABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
-#    define DISABLE_RGB_MATRIX_CYCLE_OUT_IN
-#    define DISABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
-#    define DISABLE_RGB_MATRIX_CYCLE_PINWHEEL
-#    define DISABLE_RGB_MATRIX_CYCLE_SPIRAL
-#    define DISABLE_RGB_MATRIX_DUAL_BEACON
-#    define DISABLE_RGB_MATRIX_RAINBOW_BEACON
-#    define DISABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+// #    define DISABLE_RGB_MATRIX_ALPHAS_MODS
+// #    define DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+// #    define DISABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
+// #    define DISABLE_RGB_MATRIX_BREATHING
+// #    define DISABLE_RGB_MATRIX_BAND_SAT
+// #    define DISABLE_RGB_MATRIX_BAND_VAL
+// #    define DISABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
+// #    define DISABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
+// #    define DISABLE_RGB_MATRIX_BAND_SPIRAL_SAT
+// #    define DISABLE_RGB_MATRIX_BAND_SPIRAL_VAL
+// #    define DISABLE_RGB_MATRIX_CYCLE_ALL
+// #    define DISABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+// #    define DISABLE_RGB_MATRIX_CYCLE_UP_DOWN
+// #    define DISABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+// #    define DISABLE_RGB_MATRIX_CYCLE_OUT_IN
+// #    define DISABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+// #    define DISABLE_RGB_MATRIX_CYCLE_PINWHEEL
+// #    define DISABLE_RGB_MATRIX_CYCLE_SPIRAL
+// #    define DISABLE_RGB_MATRIX_DUAL_BEACON
+// #    define DISABLE_RGB_MATRIX_RAINBOW_BEACON
+// #    define DISABLE_RGB_MATRIX_RAINBOW_PINWHEELS
 
-#    define DISABLE_RGB_MATRIX_RAINDROPS
-#    define DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+// #    define DISABLE_RGB_MATRIX_RAINDROPS
+// #    define DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
 
 #    define DISABLE_RGB_MATRIX_HUE_BREATHING
 #    define DISABLE_RGB_MATRIX_HUE_PENDULUM
 #    define DISABLE_RGB_MATRIX_HUE_WAVE
 
-//#    define DISABLE_RGB_MATRIX_TYPING_HEATMAP
-#    define DISABLE_RGB_MATRIX_DIGITAL_RAIN
-#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
-#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE
-#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
-#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
-#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
-#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
-#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
-#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
-#    define DISABLE_RGB_MATRIX_SPLASH
-#    define DISABLE_RGB_MATRIX_MULTISPLASH
-#    define DISABLE_RGB_MATRIX_SOLID_SPLASH
-//#    define DISABLE_RGB_MATRIX_SOLID_MULTISPLASH
+// #    define DISABLE_RGB_MATRIX_TYPING_HEATMAP
+// #    define DISABLE_RGB_MATRIX_DIGITAL_RAIN
+// #    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+// #    define DISABLE_RGB_MATRIX_SOLID_REACTIVE
+// #    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+// #    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+// #    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+// #    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+// #    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+// #    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+// #    define DISABLE_RGB_MATRIX_SPLASH
+// #    define DISABLE_RGB_MATRIX_MULTISPLASH
+// #    define DISABLE_RGB_MATRIX_SOLID_SPLASH
+// #    define DISABLE_RGB_MATRIX_SOLID_MULTISPLASH
+
 //*/
 #endif
 
--- keyboards/timothy/readme.md
+++ keyboards/timothy/readme.md
@@ -4,7 +4,7 @@
 
 *A short description of the keyboard/project*
 
-* Keyboard Maintainer: [geek-rabb1t](https://github.com/geek-rabb1t)
+* Keyboard Maintainer: [geek-rabb1t](https://github.com/yourusername)
 * Hardware Supported: *The PCBs, controllers supported*
 * Hardware Availability: *Links to where you can find this hardware*
 
