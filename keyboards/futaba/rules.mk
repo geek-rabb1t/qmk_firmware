f0d1bf950d6e8ff0309a5af052987bfed2563a72
geek-rabb1t <91539491+geek-rabb1t@users.noreply.github.com>
2024-11-24 08:48:12 +0900

    Update rules.mk

--- keyboards/futaba/rules.mk
+++ keyboards/futaba/rules.mk
@@ -6,5 +6,5 @@ POINTING_DEVICE_DRIVER = custom
 
 ENCODER_MAP_ENABLE = yes
 I2C_DRIVER_REQUIRED = yes
-SRC += drivers/sensors/azoteq_iqs5xx.c
+SRC += azoteq_iqs5xx.c
 SRC += futaba_azoteq_iqs5xx_driver.c
